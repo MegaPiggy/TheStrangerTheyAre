e90754b4890778ce1b755014a95d7f8dfcd1136a
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-04-18 02:32:16 -0400

    yep ok i can just do this

--- TheStrangerTheyAre/ToolModeSwapperPatches.cs
+++ TheStrangerTheyAre/ToolModeSwapperPatches.cs
@@ -5,12 +5,18 @@ namespace TheStrangerTheyAre
     [HarmonyPatch(typeof(ToolModeSwapper))]
     public static class ToolModeSwapperPatches
     {
-        // Prevent translator from being equipped in the dream world.
+        // Prevent translator from being equipped in the following conditions
+        // - When player is looking at a ghost wall text and either of the following is true:
+        //   - Is in the dream world (since the translator shouldn't work there *cough* The Outsider *cough*)
+        //   - Knows the language (since the translator isn't needed)
         [HarmonyPostfix]
         [HarmonyPatch(nameof(ToolModeSwapper.IsNomaiTextInFocus))]
-        public static void ToolModeSwapper_IsNomaiTextInFocus_Postfix(ref bool __result)
+        public static void ToolModeSwapper_IsNomaiTextInFocus_Postfix(ToolModeSwapper __instance, ref bool __result)
         {
-            __result = __result && !PlayerState.InDreamWorld();
+            var text = __instance._firstPersonManipulator.GetFocusedNomaiText();
+            var blockGhostWall = PlayerState.InDreamWorld() || StrangerTextHandlerTSTA.KnowsLanguage();
+
+            __result = __result && (!blockGhostWall || text is not GhostWallText);
         }
     }
 }
